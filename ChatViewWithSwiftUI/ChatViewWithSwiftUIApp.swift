326f8857b00104c93a98e72745fe2f4f83addebd
ppppppsmash <pppppp.work@gmail.com>
2024-11-13 20:36:54 +0900

    Initial Commit

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUIApp.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by みんみん on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
