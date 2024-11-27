6f80cd7c0d6aec7d2cec76abe026e81cb6d61e00
ppppppsmash <pppppp.work@gmail.com>
2024-11-27 22:11:32 +0900

    refactor: enhance message area ui

--- ChatViewWithSwiftUI/Views/MessageRow.swift
+++ ChatViewWithSwiftUI/Views/MessageRow.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 struct MessageRow: View {
     var body: some View {
-        HStack {
-            Circle()
-                .frame(width: 60, height: 60)
-            Capsule()
-                .frame(height: 60)
+        HStack(alignment: .top) {
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 48, height: 48)
+            Text("hello world!")
+                .padding()
+                .background(.white)
+                .cornerRadius(30)
+                // Text(Date().description)
+            VStack(alignment: .trailing) {
+                Spacer()
+                Text("既読")
+                Text(formattedDataString)
+            }
+            .foregroundColor(.secondary)
+            .font(.footnote)
+            Spacer()
         }
         .padding(.bottom)
+    }
+    
+    private var formattedDataString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
     }
 }
 
 #Preview {
     MessageRow()
+        .background(.cyan)
 }
